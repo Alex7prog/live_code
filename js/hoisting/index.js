04a34ff696e4f9cb786e3884fdbc61e16abb2656
Alex Sedmoy <alexsedmoy@gmail.com>
2022-08-19 15:17:40 +0300

    hoisting

---
+++ js/hoisting/index.js
@@ -0,0 +1,82 @@
+// test();
+// // testArrowFunc(); // error
+// console.log('price: ', price); // undefined
+// // console.log('price1: ', price1); // price1 is not defined
+// console.log('price2: ', price2); // undefined
+
+// testArrowFunc1(); //testArrowFunc1 is not a function
+
+// function test() {
+//   console.log('test');
+
+//   var price1 = 10;
+// }
+
+// if (true) {
+//   var price2 = 1;
+// }
+
+// const testArrowFunc = () => {
+//   console.log('testArrowFunc');
+// };
+
+// var testArrowFunc1 = () => {
+//   console.log('testArrowFunc1');
+// };
+
+// var price = 100;
+
+// console.log('price: ', price); // 100
+
+// testArrowFunc1(); // testArrowFunc1 is not a function
+
+// example 1
+// console.log(message);
+// var message = 'Hoisting is here!';
+
+// example 2
+// var a = 55;
+// console.log('a: ', a); // 55
+
+// if (a) {
+//   var a = 1;
+//   console.log('a: ', a); // 1
+// }
+
+// console.log('a: ', a); // 1
+
+// var a;
+// a =55;
+// console.log('a: ', a); // 55
+
+// if (a) {
+//   a = 1;
+//   console.log('a: ', a); // 1
+// }
+
+// console.log('a: ', a); // 1
+
+// example3
+var a = 77;
+console.log(a); // 77
+
+function print() {
+  var a = 2;
+  console.log(a); // 2
+}
+print();
+
+console.log(a); // 77
+
+// var a
+// a = 77;
+// console.log(a);
+
+// function print() {
+//   var a;
+//   a = 2;
+//   console.log(a);
+// }
+// print();
+
+// console.log(a);
